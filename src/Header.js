7981791a3f22d1c43e9fc2191006b08bc0a862e9
nesa9888 <69751034+nesa9888@users.noreply.github.com>
2021-12-17 19:59:41 +0100

    first-commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+
+const Header = ({ title }) => {
+
+    return (
+        <header>
+           <h1>{title}</h1> 
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Default Title'
+}
+
+export default Header
---
+++ src/LineItem.js
@@ -0,0 +1,19 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+    return (
+        <li className="item" key={item.id}>
+            <input type='checkbox' checked={item.checked} onChange={() => handleCheck(item.id)}/>
+            <label onDoubleClick={() => handleCheck(item.id)}>
+                {item.item} 
+            </label> 
+            <FaTrashAlt 
+                onClick={() => handleDelete(item.id)}
+                role='button' 
+                tabIndex='0'
+            />
+        </li>
+    )
+}
+
+export default LineItem
